972d4b8fc6bc21b764000b9f1d2c2ba7b5a75889
Peng Li <seudut@gmail.com>
2017-03-05 21:21:31 +0800

    update update.pl

--- cgi-bin/update.pl
+++ cgi-bin/update.pl
@@ -4,12 +4,15 @@ use strict;
 
 use CGI qw/:standard/;
 
-print `cd .. && make update`;
+my $out = `cd .. && make update 2>&1`;
+my $ret = $?;
 
 print header;
 print start_html("Update");
 print br;
 
 print "Hello world\n";
+print $ret;
+print $out;
 
 print end_html;
